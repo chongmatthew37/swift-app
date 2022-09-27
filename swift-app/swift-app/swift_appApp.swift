396bcf9a0c163648f4c0e921190afff1b0684edc
Matthew Chong <chongmatthew37@gmail.com>
2022-09-27 19:00:42 -0400

    first change

---
+++ swift-app/swift-app/swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_appApp.swift
+//  swift-app
+//
+//  Created by Matthew Chong on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
